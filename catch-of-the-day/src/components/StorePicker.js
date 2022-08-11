b85543207c17f58041e63adabacaae188512341b
Mingjun Geng <sooofeve@gmail.com>
2022-08-10 22:37:47 -0700

    WES BOS --- > click me

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,6 +2,9 @@ import React, {Fragment} from 'react';
 import { getFunName } from "./helpers"
 
 class StorePicker extends React.Component {
+    handleClick(){
+        alert('Heyyyy!')
+    }
     render(){
         // return <p>I am the store Picker!!!</p>
         // return React.createElement('p', {className:'hey'}, 'Heyyyyyoo!!!')
@@ -12,6 +15,7 @@ class StorePicker extends React.Component {
             {/* <p>Fish!</p> */}
             <form className='store-selector'>
                 <h2>Please Enter A Store!</h2>
+                <button onClick={this.handleClick}>Click me!</button>
                 {/* <input type="text" required placeholder='Store Name' defaultValue="hello" /> */}
                 <input type="text" required placeholder='Store Name' defaultValue={getFunName()} />
                 <button type='submit'>Visit Store  →</button>
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,8 +2,6 @@
 // alert('hey')
 import React from 'react';
 import { render } from 'react-dom'
-// import StorePicker from './components/StorePicker';
-// import App from './components/App'
 import Router from './components/Router';
 import "./css/style.css";
 
